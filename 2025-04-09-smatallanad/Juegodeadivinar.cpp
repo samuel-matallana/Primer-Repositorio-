8907258e5d6e9fd00e30b74206aaf344c9be6067
SamuelMatallana <smatallanad@unal.edu.co>
2025-04-09 07:33:07 -0500

    Juego de adivinar :)

---
+++ 2025-04-09-smatallanad/Juegodeadivinar.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main (void) {
+    int NUM = 10;
+    int guessed_number;
+    std::cout << " Adivina un núnmero entre 1 y 100: \n";
+    std::cin >> guessed_number;
+    return 0;
+ }
