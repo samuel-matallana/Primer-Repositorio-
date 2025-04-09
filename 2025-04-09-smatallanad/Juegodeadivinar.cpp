c3e76516f93b8ef888ee27058170042dd9a8b995
SamuelMatallana <smatallanad@unal.edu.co>
2025-04-09 08:06:26 -0500

    Jueguito con pistas B)

--- 2025-04-09-smatallanad/Juegodeadivinar.cpp
+++ 2025-04-09-smatallanad/Juegodeadivinar.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 int main (void) {
-    int NUM = 10;
+   const int NUM = 10;
     int guessed_number;
     std::cout << " Adivina un núnmero entre 1 y 100: \n";
     std::cin >> guessed_number;
+
+    if (guessed_number == NUM) {
+        std::cout << "Adivinaste!!!\n";
+    } else if(guessed_number > NUM) {
+        std::cout << "Te pasaste\n"; 
+    } else { 
+        std::cout << "Te falta\n"
+    }
     return 0;
  }
