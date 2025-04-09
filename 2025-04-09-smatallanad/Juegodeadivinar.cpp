2eba22914939fb485c1df33f811657907d60e345
SamuelMatallana <smatallanad@unal.edu.co>
2025-04-09 08:38:32 -0500

    ahora encima de todo el juego tiene un inicio y un fin, pero con un for, puede mejorar

--- 2025-04-09-smatallanad/Juegodeadivinar.cpp
+++ 2025-04-09-smatallanad/Juegodeadivinar.cpp
@@ -2,15 +2,20 @@
 int main (void) {
    const int NUM = 10;
     int guessed_number;
-    std::cout << " Adivina un núnmero entre 1 y 100: \n";
-    std::cin >> guessed_number;
+   
+    for (;;) {
+        std::cout << " Adivina un núnmero entre 1 y 100: \n";
+        std::cin >> guessed_number;
 
-    if (guessed_number == NUM) {
-        std::cout << "Adivinaste!!!\n";
-    } else if(guessed_number > NUM) {
-        std::cout << "Te pasaste\n"; 
-    } else { 
-        std::cout << "Te falta\n"
+        if (guessed_number == NUM) {
+            std::cout << "Adivinaste!!!\n";
+            break;
+          } else if(guessed_number > NUM) {
+            std::cout << "Te pasaste, pero puedes tratar de nuevo\n"; 
+         } else { 
+            std::cout << "Te falta, pero puedes tratar de nuevo\n";
+         } 
     }
+
     return 0;
  }
