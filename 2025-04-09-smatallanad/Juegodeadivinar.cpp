5f311309f80cb107947869fe668433038d2a8d6b
SamuelMatallana <smatallanad@unal.edu.co>
2025-04-11 08:58:58 -0500

    El juego ya con el inicio de random numeros

--- 2025-04-09-smatallanad/Juegodeadivinar.cpp
+++ 2025-04-09-smatallanad/Juegodeadivinar.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
-int main (void) {
-   const int NUM = 10;
-    unsigned int guessed_number = NUM+1;
-   
-    while (guessed_number!= NUM  ) {
-        std::cout << " Adivina un núnmero entre 1 y 100: \n";
-        std::cin >> guessed_number;
-        std:: cout << "Escribiste: " << guessed_number << "\n" ;
-        //Validación del rango 
-        if ( 1 >= guessed_number or guessed_number >= 100 ){
-         std:: cout << "Era entre 1 y 100 >:( \n" ;
-         continue;
-         }
+#include <random>
 
-        if (guessed_number == NUM) {
-            std::cout << "Adivinaste!!!\n";
-            break;
-          } else if(guessed_number > NUM) {
-            std::cout << "Te pasaste, pero puedes tratar de nuevo\n"; 
-         } else { 
-            std::cout << "Te falta, pero puedes tratar de nuevo\n";
-         } 
-    }
+
+// declaration
+void play(int min_val, int max_val);
+
+
+int main(void) {
+    play(1, 100);
+    play(2,22);
 
     return 0;
- }
+}
+// Implmentamos 
+void play(int success, int min_val, int max_val)
+{
+    const int NUM = success;
+    int guessed_number = NUM/2;
+
+    const int MIN = min_val;
+    const int MAX = max_val;
+
+
+
+    while(guessed_number != NUM) {
+        std::cout << "Adivina un numero entre  " << MIN << "  y " << MAX << ":\n";
+        std::cin >> guessed_number;
+        std::cout << "Ecribiste: " << guessed_number << "\n";
+
+        // validacion del rango 
+        if (guessed_number < MIN or MAX < guessed_number) {
+            std::cout << "Pero era entre 1 y 100 >:( \n";
+            continue; 
+        }
+
+        if (guessed_number == NUM) {
+            std::cout << "Adivinaste!!!!\n";
+            //break;
+        } else if (guessed_number > NUM){
+            std::cout << "Te pasaste, pero puedes volver a intentar\n";
+        } else { // es menor
+            std::cout << "Te falta, pero puedes volver a intentar\n";
+        }
+    }
+
+} 
