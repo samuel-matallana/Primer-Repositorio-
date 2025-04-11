ec36a50f4232ddb9b3c3a6bcd3d08b4e76d707dc
SamuelMatallana <smatallanad@unal.edu.co>
2025-04-11 08:18:41 -0500

    Parchando bugs parte 1

--- 2025-04-09-smatallanad/Juegodeadivinar.cpp
+++ 2025-04-09-smatallanad/Juegodeadivinar.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 int main (void) {
    const int NUM = 10;
-    int guessed_number = NUM+1;
+    unsigned int guessed_number = NUM+1;
    
     while (guessed_number!= NUM  ) {
         std::cout << " Adivina un núnmero entre 1 y 100: \n";
         std::cin >> guessed_number;
+        std:: cout << "Escribiste: " << guessed_number << "\n" ;
+        //Validación del rango 
+        if ( 1 >= guessed_number or guessed_number >= 100 ){
+         std:: cout << "Era entre 1 y 100 >:( \n" ;
+         continue;
+         }
 
         if (guessed_number == NUM) {
             std::cout << "Adivinaste!!!\n";
