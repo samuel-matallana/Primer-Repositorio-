cd7c36185059fd7c75c68a239d9fa7c8049b09f7
SamuelMatallana <smatallanad@unal.edu.co>
2025-07-02 08:55:54 -0500

    el txt 4 esta mal de resto todo funciona hasta el momento toca volver a revisar las ecuaciones

---
+++ 2025-07-02/particle.cpp
@@ -0,0 +1,14 @@
+// particle.cpp
+#include "particle.h"
+void Particle::print(void) {
+    std::cout << mass << "\t" << rad << "\t"
+              << R[0] << "\t" << R[1] << "\t" << R[2] << "\t"
+              << V[0] << "\t" << V[1] << "\t" << V[2] << "\t"
+              << F[0] << "\t" << F[1] << "\t" << F[2] << "\t";
+}
+
+
+
+//By William Oquendo
+
+//© Copyright 2024
